a8b3df058d585a751e5d72e99e539ccc3fdd5f81
Yusef_Ramy <yuseframy14@gmail.com>
2026-04-19 21:44:47 +0200

    feat: enhance InBody analysis with asynchronous processing and notification system

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -3,23 +3,44 @@
 use App\Http\Controllers\Api\Auth\AuthController;
 use App\Http\Controllers\Api\Food\NutritionController;
 use App\Http\Controllers\Api\InBody\InBodyController;
+use App\Http\Controllers\Api\Notifications\NotificationController;
+use App\Http\Controllers\Api\Profile\ProfileController;
+use App\Http\Controllers\Api\UserTarget\UserTargetController;
 use Illuminate\Support\Facades\Route;
-
-
-
 
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::post('/change-password', [AuthController::class, 'changePassword']);
-    Route::post('/forgot-password', [AuthController::class, 'forgotPassword']);
-    Route::post('/verify-otp', [AuthController::class, 'verifyOtp']);
-    Route::post('/reset-password', [AuthController::class, 'resetPassword']);
 
-    Route::post('/analyze', [InBodyController::class, 'analyze']);
+    Route::controller(AuthController::class)->group(function () {
+        Route::post('/logout', 'logout');
+        Route::post('/change-password', 'changePassword');
+        Route::post('/forgot-password', 'forgotPassword');
+        Route::post('/verify-otp', 'verifyOtp');
+        Route::post('/reset-password', 'resetPassword');
+    });
+
+    Route::prefix('inbody')->controller(InBodyController::class)->group(function () {
+            Route::post('/analyze', 'analyze');
+            Route::get('/latest', 'getLatestReport');
+        });
+
+    Route::controller(UserTargetController::class)->group(function () {
+        Route::get('/daily-target', 'getDailyTarget');
+    });
+
+    Route::prefix('profile')->controller(ProfileController::class)->group(function () {
+        Route::post('/fcm-token', 'updateFcmToken'); 
+        // Route::get('/', 'show');
+    });
+
+    Route::prefix('notifications')->controller(NotificationController::class)->group(function () {
+            Route::get('/', 'index');
+            Route::post('/read', 'markAsRead');
+            Route::delete('/clear-all', 'clearAll');
+        });
 });
 
 Route::middleware('auth:sanctum')->group(function () {
