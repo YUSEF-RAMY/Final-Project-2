339edc275d3304694ac248fa9ae3fec06c1e42f0
Yusef_Ramy <yuseframy14@gmail.com>
2026-04-27 15:44:46 +0300

    add UserProfile model and migration for user profile data management

--- Backend/app/Models/User.php
+++ Backend/app/Models/User.php
@@ -3,6 +3,9 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\UserDevices;
+use App\Models\UserProfile;
+use App\Models\UserTarget;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
