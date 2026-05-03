004e1406f09c3c012e47eeb277a64ab6ebb7aa0c
Yusef_Ramy <yuseframy14@gmail.com>
2026-05-03 23:23:11 +0300

    implement distributed observability system with custom log service

--- Backend/database/migrations/2026_03_12_150655_create_food_table.php
+++ Backend/database/migrations/2026_03_12_150655_create_food_table.php
@@ -4,13 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('food', function (Blueprint $table) {
+        Schema::create('foods', function (Blueprint $table) {
             $table->id();
             $table->string('name');
 
@@ -18,7 +19,9 @@ return new class extends Migration {
             $table->integer('protein');
             $table->integer('carbs');
             $table->integer('fat');
+            $table->string('serving_size')->default('100g');
             $table->string('category')->nullable();
+            $table->string('image')->nullable();
 
             $table->timestamps();
         });
@@ -29,6 +32,6 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('food');
+        Schema::dropIfExists('foods');
     }
 };
