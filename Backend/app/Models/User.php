2ab165782b10d54975fd268c34dfbb18ba17bae0
Yusef_Ramy <yuseframy14@gmail.com>
2026-05-03 23:23:10 +0300

    Add .gitignore, load environment variables, and create training notebook

--- Backend/app/Models/User.php
+++ Backend/app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\UserDevices;
+use App\Models\UserTarget;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -10,18 +12,21 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Sanctum\HasApiTokens;
+use App\Models\UserDevices;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasApiTokens, HasFactory, Notifiable, TwoFactorAuthenticatable;
+    use HasFactory, Notifiable, TwoFactorAuthenticatable, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
      *
      * @var list<string>
      */
+
     protected $fillable = ['name', 'email', 'password', 'profile_image'];
+
 
     /**
      * The attributes that should be hidden for serialization.
@@ -60,21 +65,16 @@ class User extends Authenticatable
         return $this->hasMany(Body_report::class);
     }
 
-    public function profile()
-    {
-        return $this->hasOne(UserProfile::class);
-    }
-
     public function target()
-    {
-        return $this->hasOne(UserTarget::class, 'user_id');
-    }
+{
+    return $this->hasOne(UserTarget::class, 'user_id'); 
+}
 
     /**
      * Get the user's initials
      */
     public function initials(): string
     {
-        return Str::of($this->name)->explode(' ')->take(2)->map(fn ($word) => Str::substr($word, 0, 1))->implode('');
+        return Str::of($this->name)->explode(' ')->take(2)->map(fn($word) => Str::substr($word, 0, 1))->implode('');
     }
 }
