fafd7809cfca62bc888c6782074ff7b97d5c4388
Yusef_Ramy <yuseframy14@gmail.com>
2026-05-03 23:23:10 +0300

    update database schema for meals and foods with enhanced attributes and constraints

--- Backend/database/migrations/2026_03_12_153750_create_meal_foods_table.php
+++ Backend/database/migrations/2026_03_12_153750_create_meal_foods_table.php
@@ -4,7 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
@@ -12,10 +13,18 @@ return new class extends Migration {
     {
         Schema::create('meal_foods', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('meal_id')->constrained('meals')->onDelete('cascade');
+            $table->foreignId('food_id')->constrained('foods')->onDelete('cascade');
 
-            $table->foreignId('meal_id');
+            $table->decimal('quantity', 8, 2)->default(1);
 
-            $table->foreignId('food_id');
+            // Snapshots
+            $table->integer('calories');
+            $table->integer('protein');
+            $table->integer('carbs');
+            $table->integer('fat');
+
+            $table->timestamps();
         });
     }
 
